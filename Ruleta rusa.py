26111f11cb0ceac32b3d2bfe36ac17d5942e4dd9
Jordi Dragan <149010761+JordiDragan@users.noreply.github.com>
2025-10-16 15:25:11 +0200

    Update Ruleta rusa.py

--- Ruleta rusa.py
+++ Ruleta rusa.py
@@ -12,9 +12,6 @@ numero = random.randint(1, 8)
 time.sleep(random.uniform(0.01, 0.2))
 numero_bala = random.randint(1, 8)
 
-print(f"Número del jugador: {numero}")
-print(f"Número de la bala: {numero_bala}")
-
 sistema = platform.system()
 
 if numero == numero_bala:
